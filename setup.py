2b9e243ff1101943aface40a7a260a3b49ed3341
Ben Best <ben@azundo.com>
2013-08-20 15:35:11 -0700

    Getting setup to include non-py files.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='dj-timelinejs',
-    version='0.1.0',
+    version='0.1.2',
     author='Ben Best',
     author_email='ben.best@gmail.com,',
     packages=['timelinejs'],
+    include_package_data=True,
     url='https://github.com/azundo/dj-timelinejs/',
     license='BSD license and MPL 2.0, see LICENSE',
     description='Support for serving TimelineJS from Django sites.',
