45856d065c0b35413b11042b6c7d2372ca372e30
Ben Best <ben@azundo.com>
2013-08-20 15:14:22 -0700

    Updates for building.

--- setup.py
+++ setup.py
@@ -7,10 +7,9 @@ setup(
     author='Ben Best',
     author_email='ben.best@gmail.com,',
     packages=['timelinejs'],
-    include_package_data=True,
     url='https://github.com/azundo/dj-timelinejs/',
     license='BSD license and MPL 2.0, see LICENSE',
     description='Support for serving TimelineJS from Django sites.',
-    long_descript=open('README.md').read(),
+    long_description=open('README.md').read(),
 )
 
